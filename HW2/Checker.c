e38df74e61c79f942a6efdf2cb934f0796f12c5f
Tatiana Gabel <Tianamg78@gmail.com>
2024-02-21 14:54:28 -0700

    Added formatting, and a bit more

--- HW2/Checker.c
+++ HW2/Checker.c
@@ -1,10 +1,25 @@
+#include <sys/types.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
 
-int main(int argc, int *argv[]){
+int main(int argc, char *argv[]){
     //will get 1 and A
-    printf("at checker");
+    pid_t pid = getpid();
 
+    int divisor = atoi(argv[1]);
+
+    int dividend = atoi(argv[2]);
+
+    printf("Checker process [%d]: Starting.\n", pid);
+    //Put the calculations here
+    //This will need a bit more in the string formatting, and possible concatenating strings
+    printf("Checker process [%d]: %d *IS* divisible by %d\n", pid,dividend, divisor);
+
+    printf("Checker process [%d]: Returning 1.\n", pid);
+    return 0;
 }
 
 
--- HW2/Coordinator.c
+++ HW2/Coordinator.c
@@ -14,28 +14,35 @@ int main(int argc, const char *argv[])
     }
     
     pid_t pid;
-    const char *divisor = argv[0];
-    //for (int i = 1; i <5 ; i++){
-        int i = 1;
+    const char *divisor = argv[1];
+    for (int i = 2; i <6 ; i++){
         const char *workingNumber = argv[i];
         pid = fork();
+        //Failed Process
         if (pid < 0){
             printf("failed to create process in loop %d \n", i);
             //TODO log errors??? process failed to be created
             exit(0);
+
+        //Child process    
         } else if (pid == 0){
-        printf("[%d] child process\n", i);
         execlp("checker","Checker", divisor, workingNumber, NULL);
         printf("Failed execlp\n");
+
+        //Parent process
         } else {
-            //wait();
-            printf( "parent [%d]\n", i);
+            printf("Coordinator: forked process with ID %d\n", pid);
+            printf( "Coordinator: waiting for process[%d]\n", pid);
+            int status;
+            wait(&status);
+            int result =WEXITSTATUS(status);
+            printf("Coordinator: child process %d returned %d.\n", pid,result);
         }
         //fork() return the value of the fork and print the PID
         //Run execlp() to load the checker code to newly created process
         //wait() until execuation of checker completes
 
-    //}
+    }
 //print PID
 //Print result
 //return success or error
