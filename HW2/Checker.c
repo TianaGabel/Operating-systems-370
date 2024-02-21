d8303c2185c20f66900632cb27d66cee37f5169c
Tatiana Gabel <Tianamg78@gmail.com>
2024-02-21 14:16:57 -0700

    Hey my code kind of works

--- HW2/Checker.c
+++ HW2/Checker.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*int main(int argc, int *argv[]){
+int main(int argc, int *argv[]){
     //will get 1 and A
     printf("at checker");
 
 }
-*/
+
 
 int is_divisable(int * a, int * b)
 {
--- HW2/Coordinator.c
+++ HW2/Coordinator.c
@@ -1,31 +1,41 @@
-#include <stdlib.h>
+#include <sys/types.h>
 #include <stdio.h>
-#include "Checker.h"
+#include <unistd.h>
+#include <stdlib.h>
 #include <sys/resource.h>
+#include <sys/wait.h>
 
 
 int main(int argc, const char *argv[]) 
 {
-    if (argc != ){
+    if (argc != 6){
         printf("incorrect number of args \n %d", argc);
         exit(0);
     }
     
+    pid_t pid;
     const char *divisor = argv[0];
-    for (int i = 1; i <5 ; i++){
+    //for (int i = 1; i <5 ; i++){
+        int i = 1;
         const char *workingNumber = argv[i];
-        if (fork() < 0){
+        pid = fork();
+        if (pid < 0){
             printf("failed to create process in loop %d \n", i);
             //TODO log errors??? process failed to be created
             exit(0);
+        } else if (pid == 0){
+        printf("[%d] child process\n", i);
+        execlp("checker","Checker", divisor, workingNumber, NULL);
+        printf("Failed execlp\n");
+        } else {
+            //wait();
+            printf( "parent [%d]\n", i);
         }
-        printf("actually made it here");
-        execlp("Checker.c", divisor, workingNumber, NULL);
         //fork() return the value of the fork and print the PID
         //Run execlp() to load the checker code to newly created process
         //wait() until execuation of checker completes
 
-    }
+    //}
 //print PID
 //Print result
 //return success or error
